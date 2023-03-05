f0c44dc08dd319eda260f4a471d5c505d9a41049
KangSuA <124429425+KangSuA@users.noreply.github.com>
2023-03-05 16:13:01 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 1440 ms, Memory: 43588 KB -BaekjoonHub

---
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void) {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n,l;
+    deque<pair<int,int>> dq;
+    cin >> n >> l;
+    for(int i=0;i<n;i++) {
+        int a;
+        cin >> a;
+        while(!dq.empty() && dq.back().second >= a)
+            dq.pop_back();
+        dq.push_back({i,a});
+        if (dq.front().first <= i-l) dq.pop_front();
+        cout << dq.front().second << " ";
+    }
+}
