e5642fe2962de1c72d6f271536ec0eb99b9cd36e
KangSuA <124429425+KangSuA@users.noreply.github.com>
2023-06-22 14:07:04 +0900

    [Silver III] Title: N과 M (1), Time: 112 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,18 @@
+N,M = map(int,input().split())
+
+visited = [False for _ in range(N+1)]
+
+def rec(m,com):
+  if m==M:
+    print(com)
+    return
+  for i in range(1,N+1):
+    if not visited[i]:
+      visited[i] = True
+      if m==0:
+        rec(m+1,str(i))
+      else:
+        rec(m+1,com+" "+str(i))
+      visited[i] = False
+  return
+rec(0,'')
