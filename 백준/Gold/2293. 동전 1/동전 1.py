3c5945d9e2aac4b9898cc1d3ae4119d3c1daac62
Sua Kang <nol.dev.sua@gmail.com>
2023-12-11 16:42:59 +0900

    [Gold V] Title: 동전 1, Time: 192 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n,k = map(int,input().split())
+coin = [int(input()) for i in range(n)]
+
+dp = [0]*(k+1)
+dp[0] = 1
+
+for c in coin:
+  for j in range(c,k+1):
+    dp[j] += dp[j-c]
+print(dp[k])
